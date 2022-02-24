6560186d5ed34ff792899e590f405a26aaac92c4
AugsutoAHP <augustohenrique.pereira@hotmail.com>
2022-02-24 15:22:59 -0300

    1

--- Lógica de Programação - JavaScript/09/carros.js
+++ Lógica de Programação - JavaScript/09/carros.js
@@ -8,15 +8,18 @@ function add() {
 
     document.getElementById("qnt").textContent = carros.length;
 
-    
+    if (carros.length == 10) {
         for (i = 0; i < carros.length; i++) {
             document.getElementById("lista").textContent = carros;
         }
-    
-
+    } else if (carros.length > 10) {
+        carros.splice(0, 10);
+        document.getElementById("qnt").textContent = '1';
+        document.getElementById("lista").textContent = '';
+    }
 }
 
-
+// Falta colocar cada elemento da lista em uma linha
 
 
 
